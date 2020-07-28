2e7f66c919ed44efe97bddfd3a7301417074c6d6
vicky <jiajun_work@126.com>
2020-07-28 08:29:01 +0800

    [WordGame][vicky] refactor: add getWordCounts.

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,16 +9,21 @@ public class WordFrequencyGame {
 
         Map<String, Integer> wordMap = calculateFrequentWords(sentence);
 
-        List<Input> list = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
-            Input input = new Input(entry.getKey(), entry.getValue());
-            list.add(input);
-        }
+        List<Input> list = getWordCounts(wordMap);
 
         list.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
         StringJoiner joiner = formatSentence(list);
         return joiner.toString();
+    }
+
+    private List<Input> getWordCounts(Map<String, Integer> wordMap) {
+        List<Input> list = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
+            Input input = new Input(entry.getKey(), entry.getValue());
+            list.add(input);
+        }
+        return list;
     }
 
     private StringJoiner formatSentence(List<Input> inputList) {
