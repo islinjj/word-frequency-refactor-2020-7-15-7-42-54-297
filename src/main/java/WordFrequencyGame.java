8c30004ee26da705eea2b454d66fc71ca9020d7a
vicky <jiajun_work@126.com>
2020-07-28 12:55:52 +0800

    [WordGame][vicky] refactor: remove sort and getWordCounts.

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -6,34 +6,12 @@ public class WordFrequencyGame {
 
         Map<String, Integer> wordMap = calculateFrequentWords(sentence);
 
-        List<Input> list = getWordCounts(wordMap);
-
-        sort(list);
-
-        StringJoiner joiner = formatSentence(list);
-        return joiner.toString();
-    }
-
-    private void sort(List<Input> list) {
-        list.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-    }
-
-    private List<Input> getWordCounts(Map<String, Integer> wordMap) {
-        List<Input> list = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
-            Input input = new Input(entry.getKey(), entry.getValue());
-            list.add(input);
-        }
-        return list;
-    }
-
-    private StringJoiner formatSentence(List<Input> inputList) {
-        StringJoiner joiner = new StringJoiner("\n");
-        for (Input w : inputList) {
-            String s = w.getValue() + " " + w.getWordCount();
-            joiner.add(s);
-        }
-        return joiner;
+        return wordMap
+                .entrySet()
+                .stream()
+                .sorted((e1,e2) -> e2.getValue() - e1.getValue())
+                .map(e -> e.getKey() + " " + e.getValue())
+                .collect(Collectors.joining("\n"));
     }
 
     private Map<String, Integer> calculateFrequentWords(String sentence) {
